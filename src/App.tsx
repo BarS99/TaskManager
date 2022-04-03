03bb73e3faaa3d484307a3578be788741f4e9b4d
Bartlomiej <bartlomiejswiech99@gmail.com>
2022-04-03 20:24:41 +0200

    Remove h1

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import React from "react";
 function App() {
   return (
     <div className="App theme-light">
-      <div className="content">
-        <h1>Test</h1>
-      </div>
+      <div className="content">Sample text</div>
     </div>
   );
 }
