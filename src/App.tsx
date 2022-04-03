e82eae90a164a21f6eaedac394fad12f314957a3
Bartlomiej <bartlomiejswiech99@gmail.com>
2022-04-03 20:21:00 +0200

    remove sample text

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,6 @@ function App() {
     <div className="App theme-light">
       <div className="content">
         <h1>Test</h1>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore sint
-        officia fugiat, a voluptatem rem porro laboriosam minima, quo itaque
-        assumenda illo repellat natus perspiciatis libero, ad ducimus aliquid
-        pariatur. Eius quidem alias illum similique eveniet, aliquid, animi
-        harum reiciendis quia, omnis est modi facilis autem consequuntur? Soluta
-        asperiores minus vel fuga id adipisci deserunt repellendus similique.
-        Illo, veniam laudantium. Autem velit consequuntur doloribus? Quis
-        pariatur doloremque, ad quam consequuntur et! Labore aliquid alias
-        aspernatur debitis. Magni fugiat esse cumque qui nam, officiis voluptas
-        facilis reprehenderit odio ipsum, laboriosam dicta. Quod obcaecati quae
-        libero quasi iste ratione, culpa fuga quaerat quas fugiat vitae unde
-        eos, quisquam laboriosam illum doloribus inventore nisi, molestiae eaque
-        facere! Eaque consequuntur impedit minima! Dolores, voluptatibus.
-        Perspiciatis sed ullam incidunt facilis, asperiores ipsam minima,
-        dignissimos tenetur modi temporibus aspernatur vero beatae totam,
-        corporis amet nobis. Consectetur aliquid id illum veniam consequuntur
-        sit tempore labore animi? Enim?
       </div>
     </div>
   );
